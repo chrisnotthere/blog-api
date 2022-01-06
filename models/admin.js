0afc3c6ed98ce01edb3eafec8a2606b950a2aedc
chris <mr.chrisnash@gmail.com>
2022-01-06 10:10:52 -0800

    hash password with bcrypt

--- models/admin.js
+++ models/admin.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
 var AdminSchema = new Schema({
   firstname: { type: String, required: true },
@@ -7,6 +8,12 @@ var AdminSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+});
+
+AdminSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
 });
 
 module.exports = mongoose.model("Admin", AdminSchema);
