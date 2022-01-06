c8da051fc405849efb11ce1e6a4557baf6e3f32f
chris <mr.chrisnash@gmail.com>
2022-01-05 16:38:25 -0800

    blogController can show all blogs, show a single blog by id, and handles add a comment

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -6,26 +6,81 @@ var Post = require("../models/post");
 // Display list of all Posts.
 exports.blog_list = (req, res, next) => {
 
-  res.send('NOT IMPLEMENTED: display list of all blogs');
+  Post.find()
+    .sort([["date", "ascending"]])
+    .exec(function (err, results) {
+      if (err) {
+        console.log('there was an error');
+        return next(err);
+      }
+      //Successful, so send data
+      console.log('success');
+      res.json({ results })
 
-  // Post.find()
-  //   .sort([["date", "ascending"]])
-  //   .exec(function (err, posts_list) {
-  //     if (err) {
-  //       console.log('there was an error');
-  //       return next(err);
-  //     }
-  //     //Successful, so send data
-  //     console.log('success');
-  //     res.json({ posts_list })
-
-  //   });
+    });
 };
 
 exports.blog_get = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: show a single blog');
-}
+
+  Post.findById(req.params.id)
+    .exec(function (err, results) {
+      if (err) {
+        console.log('there was an error');
+        return next(err);
+      }
+      if (results.title == null) {
+        // No results.
+        console.log('no blog found');
+        const err = new Error("Blog not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so send data.
+      console.log('success');
+      res.json({ results })
+    });
+};
 
 exports.comment_post = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: create a comment');
-}
+  // find the blog then add new comment to comments array
+  //find the blog
+  Post.findById(req.params.id)
+  .exec(function (err, result) {
+    if (err) {
+      console.log('there was an error');
+      return next(err);
+    }
+    if (result.title == null) {
+      // No results.
+      console.log('no blog found');
+      const err = new Error("Blog not found");
+      err.status = 404;
+      return next(err);
+    }
+    // blog was found, now add comment to comments array
+
+    // Create a comment object with data from user.
+    const comment = {
+      username: req.body.username,
+      content: req.body.content,
+      date: Date.now(),
+    };
+    // push comment onto blog's comment array
+    result.comments.push(comment);
+
+    // save
+    Post.findByIdAndUpdate(
+      req.params.id,
+      result,
+      {},
+      function (err, thepost) {
+        if (err) {
+          return next(err);
+        }
+        // Successful - send updated blog
+        console.log('success');
+        res.json({ result })
+      }
+    );
+  });
+};
