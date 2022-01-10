66b146111ed76adf5909b123257941c58921fcf1
chris <mr.chrisnash@gmail.com>
2022-01-10 15:40:29 -0800

    cors

--- app.js
+++ app.js
@@ -32,7 +32,13 @@ app.use(bodyParser.json())
 app.use(compression()); //Compress all routes
 app.use(helmet()); //helps protet against vulnerabilites
 app.use(cors());
-app.options('*', cors());
+app.use(function(req, res, next) {
+  //res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "x-requested-with, Content-Type, origin, authorization, Accepts, accept, client-security-token, access-control-allow-headers");
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/blog', blogRouter);
