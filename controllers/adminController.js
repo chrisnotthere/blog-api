aa38b89e65c7b7e63c6a32a103dc50f89e187e21
chris <mr.chrisnash@gmail.com>
2022-01-10 10:23:39 -0800

    test

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -38,6 +38,8 @@ exports.login_post = (req, res, next) => {
 
     //we need to hash the req.password and then compare with db password
     const hashedSentPassword = bcrypt.hash(password, 10);
+    console.log(hashedSentPassword);
+    console.log(user.password);
 
     //check if password in DB matches
     bcrypt.compare(hashedSentPassword, user.password, (err, response) => {
