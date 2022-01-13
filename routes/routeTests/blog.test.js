a654de4e6c1eadf484e8bd4529718aed0b4fdabc
chris <mr.chrisnash@gmail.com>
2022-01-12 16:54:43 -0800

    working on tests

--- routes/routeTests/blog.test.js
+++ routes/routeTests/blog.test.js
@@ -1,12 +1,34 @@
 const blog = require("../blog");
 const request = require("supertest");
 const express = require("express");
-const app = require('../../app');
+//const app = require('../../app');
+
+const initializeMongoServer = require('../../config/mongoConfigTesting');
+const app = express();
+initializeMongoServer();
+
+var Post = require("../../models/post");
 
 app.use(express.urlencoded({ extended: false }));
 app.use("/blog", blog);
 
 describe('GET /blog', () => {
+
+  beforeAll(() => {
+
+    const postDetail = {
+      "title" :"New Blog Title",
+      "date" : 1641421215076,
+      "content" : "lorem ipsum 123345678910...",
+      "author" : "61dc82433f1943a7eb665fa9",
+      "published" : true,
+      "img" : "image.png",
+      "comments" : [],
+    }
+    var post = new Post(postDetail);
+    post.save();
+
+  })
 
   test("blog returns 'results' as json and 200 status code", done => {
     request(app)
@@ -16,10 +38,13 @@ describe('GET /blog', () => {
       .expect(200)
       .then(response => {
         expect(response.body.results).toBeDefined()
+        expect(response.body.results[0].title).toBe('New Blog Title')
         done();
     })
     .catch(err => done(err))
   });
+
+
 
 })
 
