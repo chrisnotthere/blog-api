fc65d31e9c2a510327e0fd03fd1c3fbecd7ad64e
chris <mr.chrisnash@gmail.com>
2022-01-14 11:51:41 -0800

    playing with cors config

--- app.js
+++ app.js
@@ -22,19 +22,22 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  // res.header("Access-Control-Allow-Credentials", "true");
-  //res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS");
-  //res.header("Access-Control-Allow-Headers", "x-requested-with, Content-Type, origin, authorization, Accepts, accept, client-security-token, access-control-allow-headers");
-  next();
-});
-
 app.use(logger('dev'));
 app.use(bodyParser.json())
 app.use(compression()); //Compress all routes
 app.use(helmet()); //helps protet against vulnerabilites
-app.use(cors());
+//app.use(cors());
+
+const whitelist = ['http://localhost:3000', 'http://otherwebsite.com'];
+const corsOptions = {
+  credentials: true, // This is important.
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin))
+      return callback(null, true)
+      callback(new Error('Not allowed by CORS'));
+  }
+}
+app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
 app.use('/blog', blogRouter);
