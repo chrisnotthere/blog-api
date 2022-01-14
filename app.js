45565edfb049f80ce03bb80b2f3ff4a0942817c6
chris <mr.chrisnash@gmail.com>
2022-01-14 11:33:11 -0800

    cleanup comments

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const cors = require('cors');
 
 // Set up mongoose connection
 require('./config/mongoConfig');
-// require('./config/mongoConfigTesting');  //uses mongodb-memory-server for testing purposes
 
+// define routes/controllers
 const indexRouter = require('./routes/index');
 const blogRouter = require('./routes/blog');
 const adminRouter = require('./routes/admin');
@@ -22,31 +22,11 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   // res.header("Access-Control-Allow-Credentials", "true");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "x-requested-with, Content-Type, origin, authorization, Accepts, accept, client-security-token, access-control-allow-headers");
-//   next();
-// });
-
 app.use(logger('dev'));
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(compression()); //Compress all routes
 app.use(helmet()); //helps protet against vulnerabilites
 app.use(cors());
-const whitelist = ['http://localhost:3000', 'http://otherwebsite.com'];
-const corsOptions = {
-  credentials: true, // This is important.
-  origin: (origin, callback) => {
-    if(whitelist.includes(origin))
-      return callback(null, true)
-      callback(new Error('Not allowed by CORS'));
-  }
-}
-// app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
 app.use('/blog', blogRouter);
